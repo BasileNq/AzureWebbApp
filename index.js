329510fe6e554e2e9d66fa7dfda10b1da1ccc0bb
basile <basile@kali.BOSS>
2021-04-29 10:18:26 +0200

    injection breach added

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const http = require('http');
+const url = require('url');
 
+const res = () =>{
+
+}
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    const queryData = url.parse(request.url, true).query;
+    response.writeHead(200, {"Content-Type": "text/html"});
+    if (queryData.name){
+        response.end("Salut " + queryData.name);
+    } else {
+        response.end("Quel est votre nom ?");
+    }
+    
 });
 
 const port = process.env.PORT || 1337;
